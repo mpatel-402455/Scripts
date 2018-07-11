31b5398c7cfb9a28679be421c99392237952588f
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:08:21 -0400

    Add files via upload

---
+++ get-LessThen_N_FreeSpace.ps1
@@ -0,0 +1,19 @@
+﻿$computers = "Srv01", "Srv02"
+foreach($computer in $computers)
+{
+    $drives = Get-WmiObject -ComputerName $computer Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3}
+    foreach($drive in $drives)
+    {
+        $obj = new-object psobject -Property @{
+                   ComputerName = $computer
+                   Drive = $drive.DeviceID
+                   Size  = $drive.size / 1GB
+                   Free  = $drive.freespace / 1GB
+                   PercentFree = $drive.freespace / $drive.size * 100
+               }
+        if ($obj.PercentFree -lt 100) {
+            $obj | Format-Table ComputerName,Drive,Size,Free,PercentFree
+            $i++
+        }
+    }
+}
