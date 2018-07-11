e1589a7efc54900770eaf946d7689162b267e9c4
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:09:05 -0400

    Get Physical Memory slot info
    Get Physical Memory slot info

---
+++ Get-MemoryInfo.ps1
@@ -0,0 +1,18 @@
+﻿$memorytype = "Unknown", "Other", "DRAM", "Synchronous DRAM", "Cache DRAM",
+	"EDO", "EDRAM", "VRAM", "SRAM", "RAM", "ROM", "Flash", "EEPROM", "FEPROM",
+
+	"EPROM", "CDRAM", "3DRAM", "SDRAM", "SGRAM", "RDRAM", "DDR", "DDR-2"
+
+	$formfactor = "Unknown", "Other", "SIP", "DIP", "ZIP", "SOJ", "Proprietary",
+
+	"SIMM", "DIMM", "TSOP", "PGA", "RIMM", "SODIMM", "SRIMM", "SMD", "SSMP",
+
+	"QFP", "TQFP", "SOIC", "LCC", "PLCC", "BGA", "FPBGA", "LGA"
+
+	$col1 = @{Name='Size (GB)'; Expression={ $_.Capacity/1GB } }
+
+	$col2 = @{Name='Form Factor'; Expression={$formfactor[$_.FormFactor]} }
+
+	$col3 = @{Name='Memory Type'; Expression={ $memorytype[$_.MemoryType] } }
+
+	Get-WmiObject Win32_PhysicalMemory | Select-Object BankLabel, $col1, $col2, $col3
