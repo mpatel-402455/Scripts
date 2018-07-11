4c1dd4bc4ab0f678d2d121d43dbe6db463ae13e4
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 18:52:48 -0400

    Change DNS IP
    Change DNS IP

---
+++ Change-DNS-IP-Remotely.ps1
@@ -0,0 +1,27 @@
+﻿$DNSIP = "10.14.48.15", "10.1.47.18", "10.14.48.21"
+$DNSIP2 = "10.1.47.19","10.1.47.18","10.14.48.15"  
+
+
+$ServerIP = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName "srv01" -Filter "IPEnabled = True" 
+$ServerIP.SetDNSServerSearchOrder($DNSIP)
+
+
+
+#----CHECK
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName "serv01.subdomain.domain.ca" -Filter "IPEnabled = True" | Select-Object -Property PSComputerName, DNSServerSearchOrder, IPAddress | Format-Table -AutoSize
+#----
+
+
+#--------check and update in loop
+$servers = "", ""
+
+ foreach ($server in $Servers)
+    {
+        Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $server -Filter "IPEnabled = True" | Select-Object -Property PSComputerName, DNSServerSearchOrder, IPAddress | Format-Table -AutoSize
+        $ServerIP = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $server -Filter "IPEnabled = True" 
+        $ServerIP.SetDNSServerSearchOrder($DNSIP)
+        Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $server -Filter "IPEnabled = True" | Select-Object -Property PSComputerName, DNSServerSearchOrder, IPAddress | Format-Table -AutoSize
+    }
+
+
+#--------------
