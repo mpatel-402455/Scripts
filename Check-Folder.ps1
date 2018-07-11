17e9c4eb536015818005c7321ba97f6443221261
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 18:54:53 -0400

    Check if folder exist
    Check if specified folder exist on share

---
+++ Check-Folder.ps1
@@ -0,0 +1,23 @@
+﻿$ServerNames = Import-Csv -Path C:\DATA\_Scritps\Input-Files\2001_0801_servers_List.csv
+
+
+foreach ($Server in $ServerNames)
+    {
+        $ServerName = $Server.HostName
+         #"\\$ServerName\Apps\Orant\NETWORK\ADMIN"
+
+        Write-Host "Server: $ServerName" 
+
+        $Folder =Test-Path -Path "\\$ServerName\Apps\Orant\NETWORK\ADMIN"
+
+        if ( $Folder -eq $true)
+           
+            {
+                Write-host "Folder \Apps\Orant\NETWORK\ADMIN exist on: $ServerName"  -ForegroundColor Green
+            }
+
+        elseif ($Folder -eq $false)
+             {
+                Write-host "Folder \Apps\Orant\NETWORK\ADMIN DOES NOT exist on: $ServerName"  -ForegroundColor Red
+             }
+    }
