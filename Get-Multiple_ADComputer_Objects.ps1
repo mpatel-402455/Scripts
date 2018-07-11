159002199976beab60603cad80f2e8b9e78d6652
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:11:35 -0400

    Get AD Computer Object Info
    Get multiple AD Computer Object Information

---
+++ Get-Multiple_ADComputer_Objects.ps1
@@ -0,0 +1,9 @@
+﻿$ServerName = ("SRV01","Srv02","Srv03","Srv04")
+  
+
+foreach ($Server in $ServerName) 
+    {  
+       # Get-ADComputer -Identity  $Server 
+
+        Get-ADComputer -Identity  $Server | Select-Object -Property DistinguishedName,DNSHostName,Enabled,Name,ObjectClass,ObjectGUID,SamAccountName,SID
+    } 
