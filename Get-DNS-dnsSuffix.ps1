09b6991291bfccbfedf462424f788ff975bf9ee5
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:02:24 -0400

    Get DNS Suffix
    Get DNS Suffix

---
+++ Get-DNS-dnsSuffix.ps1
@@ -0,0 +1,6 @@
+﻿param(
+    [string]$ComputerName = $Env:COMPUTERNAME
+    #[string]$Credential
+    )
+
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $ComputerName -Filter "IPEnabled = True" | Select-Object -Property PSComputerName, DNSServerSearchOrder, IPAddress, DNSDomainSuffixSearchOrder | Format-Table -AutoSize
