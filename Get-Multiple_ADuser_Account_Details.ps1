4b6c8ac58bb0be2467537bc4b5fd7d8db9f64e91
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:12:41 -0400

    Get AD user account Info
    Get multiple AD USER Accounts details

---
+++ Get-Multiple_ADuser_Account_Details.ps1
@@ -0,0 +1,7 @@
+﻿$users= ("user1", "user2", "user3","user4","user5")
+
+
+foreach ($user in $users)
+    {
+        Get-ADUser $user -Properties * | Select-Object -Property cn, samaccountname, AccountExpirationDate, LockedOut, Enabled,PasswordNeverExpires,PasswordLastSet,Description
+    }
