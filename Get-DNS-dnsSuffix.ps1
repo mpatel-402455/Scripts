b5712bf4f5711a4e3ac2c290652bf2a2be656a07
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:35:15 -0400

    Update Get-DNS-dnsSuffix.ps1

--- Get-DNS-dnsSuffix.ps1
+++ Get-DNS-dnsSuffix.ps1
@@ -4,3 +4,29 @@
     )
 
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $ComputerName -Filter "IPEnabled = True" | Select-Object -Property PSComputerName, DNSServerSearchOrder, IPAddress, DNSDomainSuffixSearchOrder | Format-Table -AutoSize
+
+
+<#
+
+
+## FOLLOWING works locally 
+# 
+([WMIClass]"Win32_NetworkAdapterConfiguration").SetDNSSuffixSearchOrder(("prod.lab.ca", "lab.root", "lab.ca", "int.lab.ca", "dev.lab.ca", "production.lab.ca", "qalab.ca"))
+$a = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
+$a.DNSDomainSuffixSearchOrder
+
+
+
+#2 
+([WMIClass]"Win32_NetworkAdapterConfiguration").SetDNSSuffixSearchOrder(("prod.lab.ca", "domain.root", "lab.ca", "int.lab.ca", "dev.lab.ca", "production.lab.ca", "qalab.ca"))
+$a = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
+$a.DNSDomainSuffixSearchOrder
+
+
+
+#following are DOS command to change dns suffix on Windows 2003
+
+reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v "SearchList"
+
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v "SearchList" /d "prod.lab.ca,lab.root,lab.ca,int.lab.ca,dev.lab.ca" /f
+#>
