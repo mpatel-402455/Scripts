ceec5204f440399967038ba344ddfcde2b0994cc
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:13:21 -0400

    Get Operating System (OS) Name
    Get Operating System (OS) Name

---
+++ Get-OperatingSystemName.ps1
@@ -0,0 +1,6 @@
+﻿param (
+      [string]$ComputerName = $env:COMPUTERNAME
+      )
+$OS=(Get-WmiObject -Class Win32_OperatingSystem -ComputerName $ComputerName)
+$OS
+Write-Host "Operating System: " $OS.caption
