3c7b103ee6d2887bf426267bebae47c5e6bee75e
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 21:47:01 -0400

    add

---
+++ Registry/Get-RegistryKeyPropertiesAndValues.ps1
@@ -0,0 +1,84 @@
+﻿
+
+Function Get-RegistryKeyPropertiesAndValues
+
+{
+
+  <#
+
+   .Synopsis
+
+    This function accepts a registry path and returns all reg key properties and values
+
+   .Description
+
+    This function returns registry key properies and values.
+
+   .Example
+
+    Get-RegistryKeyPropertiesAndValues -path 'HKCU:\Volatile Environment'
+
+    Returns all of the registry property values under the \volatile environment key
+
+   .Parameter path
+
+    The path to the registry key
+
+   .Notes
+
+    NAME:  Get-RegistryKeyPropertiesAndValues
+
+    AUTHOR: ed wilson, msft
+
+    LASTEDIT: 05/09/2012 15:18:41
+
+    KEYWORDS: Operating System, Registry, Scripting Techniques, Getting Started
+
+    HSG: 5-11-12
+
+   .Link
+
+     Http://www.ScriptingGuys.com/blog
+
+ #Requires -Version 2.0
+
+ #>
+
+ Param(
+
+  [Parameter(Mandatory=$true)]
+
+  [string]$path)
+
+ Push-Location
+
+ Set-Location -Path $path
+
+ Get-Item . |
+
+ Select-Object -ExpandProperty property |
+
+ ForEach-Object {
+
+ New-Object psobject -Property @{"property"=$_;
+
+    "Value" = (Get-ItemProperty -Path . -Name $_).$_}}
+
+ Pop-Location
+
+} #end function Get-RegistryKeyPropertiesAndValues
+
+<#
+1. Enumerating registry property values:
+
+2. Use the Push-Location cmdlet to store the current working location.
+
+3. Use the Set-Location cmdlet to change the current working location to the appropriate registry drive.
+
+4. Use the Get-Item cmdlet to retrieve the properties of the registry key.
+
+5. Pipe the registry properties through the ForEach-Object cmdlet.
+
+6. In the script block of the ForEach-Object cmdlet, use the Get-ItemProperty cmdlet to retrieve the property values.
+Return to the original working location by using the Pop-Location cmdlet.
+#>
---
+++ Registry/Update_Registry.ps1
@@ -0,0 +1,49 @@
+﻿                     <####################################################################################
+                     #    .SYNOPSIS                                                                     #
+                     #       Create new Registry key with value.                                        #
+                     #    .DESCRIPTION                                                                  #
+                     #       Create new Registry key with value.                                        #
+                     #                                                                                  #
+                     #    .NOTES                                                                        #
+                     #       File Name      : Update_Registry.ps1                                       #
+                     #       Author         : Manish Patel                                              #
+                     #       Script Version : 1.0                                                       #
+                     #       Last Modified  : December 13, 2017                                         #
+                     #       Prerequisite   : PowerShell.                                               #
+                     #       Author         : Manish Patel                                              #
+                     ####################################################################################>
+
+
+# https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/working-with-registry-keys?view=powershell-5.1
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-itemproperty?view=powershell-5.1
+
+
+
+#$ServerNames = "04693PULD","04696PULD","04694PULD","04645PULD","04643PULD","04644PULD","04642PULD","04641PULD","04692PULD","04640PULD","04644MILQ","04643MILQ","04645MILQ","04641MILQ","04642MILQ","04640MILQ"
+$ServerNames = Get-Content -Path "C:\DATA\MyScripts\In-Put-Files\Test_ServerList.txt"
+
+#$RegPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX"
+$RegPath = HOSTNAME
+foreach ($server in $ServerNames)
+    {
+        
+        Write-Host "Working on server: $server"
+
+            if ((Invoke-Command -ComputerName $server -ScriptBlock {Test-Path -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX'}) -eq $false )
+                {
+                    Write-Host "The registry key not found on $server" -ForegroundColor DarkMagenta `n
+                    Write-Host "Creating new registry Key:" `n -ForegroundColor Green
+
+                    #creating new key
+                    (Invoke-Command -ComputerName $server -ScriptBlock {New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl' -Name "FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX"})
+                    (Invoke-Command -ComputerName $server -ScriptBlock {New-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX' -Name "iexplore.exe" -Value 1 -PropertyType "DWord"})
+                }
+
+            if ((Invoke-Command -ComputerName $server -ScriptBlock {Test-Path -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX'}) -eq $true )
+                {
+                    Write-Host "The registry key found on $server" -ForegroundColor Green `n
+                    #Getting Registery info
+                    Invoke-Command -ComputerName $server -ScriptBlock {Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX'}
+                }
+
+    }
