1664a84e4391a48a8ff3668574497c825e7083dd
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 21:46:53 -0400

    move to AD

--- Get-Multiple_ADComputer_Objects.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ServerName = ("SRV01","Srv02","Srv03","Srv04")
-  
-
-foreach ($Server in $ServerName) 
-    {  
-       # Get-ADComputer -Identity  $Server 
-
-        Get-ADComputer -Identity  $Server | Select-Object -Property DistinguishedName,DNSHostName,Enabled,Name,ObjectClass,ObjectGUID,SamAccountName,SID
-    } 
--- VM-Uptime.ps1
+++
@@ -1,7 +0,0 @@
-$LastBootProp = @{
-  Name = 'LastBootTime'
-    Expression = {
-      ( Get-Date ) - ( New-TimeSpan -Seconds $_.Summary.QuickStats.UptimeSeconds )
- }
-}
-Get-View -ViewType VirtualMachine -Property Name, Summary.QuickStats.UptimeSeconds | Select Name, $LastBootProp
